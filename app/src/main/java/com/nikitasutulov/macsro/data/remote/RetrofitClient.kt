291a50c503cd2d727577068a213ea13929679d15
Nikita Sutulov <notsid212@gmail.com>
2025-03-13 10:32:44 +0200

    refactored RetrofitClient
    Improved code readability and made it more compact.

--- app/src/main/java/com/nikitasutulov/macsro/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/nikitasutulov/macsro/data/remote/RetrofitClient.kt
@@ -11,112 +11,33 @@ object RetrofitClient {
     private const val UTILS_BASE_URL = "http://192.168.0.105:5031"
     private const val OPERATIONS_BASE_URL = "http://192.168.0.105:5160"
 
-    private val authRetrofitClient: Retrofit.Builder by lazy {
+    private val gsonConverterFactory = GsonConverterFactory.create()
+
+    private fun createRetrofit(baseUrl: String): Retrofit =
         Retrofit.Builder()
-            .baseUrl(AUTH_BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-    }
-
-    private val utilsRetrofitClient: Retrofit.Builder by lazy {
-        Retrofit.Builder()
-            .baseUrl(UTILS_BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-    }
-
-    private val operationsRetrofitClient: Retrofit.Builder by lazy {
-        Retrofit.Builder()
-            .baseUrl(OPERATIONS_BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-    }
-
-    val authApi: AuthApi by lazy {
-        authRetrofitClient
+            .baseUrl(baseUrl)
+            .addConverterFactory(gsonConverterFactory)
             .build()
-            .create(AuthApi::class.java)
-    }
 
-    val roleApi: RoleApi by lazy {
-        authRetrofitClient
-            .build()
-            .create(RoleApi::class.java)
-    }
+    private val authRetrofitClient: Retrofit by lazy { createRetrofit(AUTH_BASE_URL) }
+    private val utilsRetrofitClient: Retrofit by lazy { createRetrofit(UTILS_BASE_URL) }
+    private val operationsRetrofitClient: Retrofit by lazy { createRetrofit(OPERATIONS_BASE_URL) }
 
-    val userApi: UserApi by lazy {
-        authRetrofitClient
-            .build()
-            .create(UserApi::class.java)
-    }
+    val authApi: AuthApi by lazy { authRetrofitClient.create(AuthApi::class.java) }
+    val roleApi: RoleApi by lazy { authRetrofitClient.create(RoleApi::class.java) }
+    val userApi: UserApi by lazy { authRetrofitClient.create(UserApi::class.java) }
 
-    val districtApi: DistrictApi by lazy {
-        utilsRetrofitClient
-            .build()
-            .create(DistrictApi::class.java)
-    }
+    val districtApi: DistrictApi by lazy { utilsRetrofitClient.create(DistrictApi::class.java) }
+    val measurementUnitApi: MeasurementUnitApi by lazy { utilsRetrofitClient.create(MeasurementUnitApi::class.java) }
+    val resourceApi: ResourceApi by lazy { utilsRetrofitClient.create(ResourceApi::class.java) }
+    val resourceMeasurementUnitApi: ResourceMeasurementUnitApi by lazy { utilsRetrofitClient.create(ResourceMeasurementUnitApi::class.java) }
 
-    val measurementUnitApi: MeasurementUnitApi by lazy {
-        utilsRetrofitClient
-            .build()
-            .create(MeasurementUnitApi::class.java)
-    }
-
-    val resourceApi: ResourceApi by lazy {
-        utilsRetrofitClient
-            .build()
-            .create(ResourceApi::class.java)
-    }
-
-    val resourceMeasurementUnitApi: ResourceMeasurementUnitApi by lazy {
-        utilsRetrofitClient
-            .build()
-            .create(ResourceMeasurementUnitApi::class.java)
-    }
-
-    val eventApi: EventApi by lazy {
-        operationsRetrofitClient
-            .build()
-            .create(EventApi::class.java)
-    }
-
-    val eventStatusApi: EventStatusApi by lazy {
-        operationsRetrofitClient
-            .build()
-            .create(EventStatusApi::class.java)
-    }
-    
-    val eventTypeApi: EventTypeApi by lazy {
-        operationsRetrofitClient
-            .build()
-            .create(EventTypeApi::class.java)
-    }
-
-    val groupApi: GroupApi by lazy {
-        operationsRetrofitClient
-            .build()
-            .create(GroupApi::class.java)
-    }
-
-    val operationTaskApi: OperationTaskApi by lazy {
-        operationsRetrofitClient
-            .build()
-            .create(OperationTaskApi::class.java)
-    }
-
-    val operationTaskStatusApi: OperationTaskStatusApi by lazy {
-        operationsRetrofitClient
-            .build()
-            .create(OperationTaskStatusApi::class.java)
-    }
-
-    val operationWorkerApi: OperationWorkerApi by lazy {
-        operationsRetrofitClient
-            .build()
-            .create(OperationWorkerApi::class.java)
-    }
-
-    val resourcesEventApi: ResourcesEventApi by lazy {
-        operationsRetrofitClient
-            .build()
-            .create(ResourcesEventApi::class.java)
-
-    }
+    val eventApi: EventApi by lazy { operationsRetrofitClient.create(EventApi::class.java) }
+    val eventStatusApi: EventStatusApi by lazy { operationsRetrofitClient.create(EventStatusApi::class.java) }
+    val eventTypeApi: EventTypeApi by lazy { operationsRetrofitClient.create(EventTypeApi::class.java) }
+    val groupApi: GroupApi by lazy { operationsRetrofitClient.create(GroupApi::class.java) }
+    val operationTaskApi: OperationTaskApi by lazy { operationsRetrofitClient.create(OperationTaskApi::class.java) }
+    val operationTaskStatusApi: OperationTaskStatusApi by lazy { operationsRetrofitClient.create(OperationTaskStatusApi::class.java) }
+    val operationWorkerApi: OperationWorkerApi by lazy { operationsRetrofitClient.create(OperationWorkerApi::class.java) }
+    val resourcesEventApi: ResourcesEventApi by lazy { operationsRetrofitClient.create(ResourcesEventApi::class.java) }
 }
