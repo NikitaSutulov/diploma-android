f33ef29404db42a92d413e9849284641e81d5748
Nikita Sutulov <notsid212@gmail.com>
2025-04-21 22:10:02 +0300

    added proper back stack managing
    Implemented popping back stack and navigating to the previous fragment where possible and logical.

--- app/src/main/java/com/nikitasutulov/macsro/MainActivity.kt
+++ app/src/main/java/com/nikitasutulov/macsro/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.nikitasutulov.macsro
 
 import android.os.Bundle
+import androidx.activity.addCallback
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,26 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        onBackPressedDispatcher.addCallback(this) {
+            val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+            val navController = navHostFragment.navController
+            val isOnLoginFragment = navController.currentDestination?.id == R.id.loginFragment
+            val isOnRegisterFragment = navController.currentDestination?.id == R.id.registerFragment
+            val isOnEventsFragment = navController.currentDestination?.id == R.id.eventsFragment
+            if (isOnLoginFragment || isOnRegisterFragment || isOnEventsFragment) {
+                showExitConfirmationDialog()
+            } else {
+                navController.popBackStack()
+            }
+        }
+    }
+
+    private fun showExitConfirmationDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Exit the app")
+            .setMessage("Do you want to exit?")
+            .setPositiveButton("Yes") { _, _ -> finish() }
+            .setNegativeButton("No", null)
+            .show()
     }
 }
