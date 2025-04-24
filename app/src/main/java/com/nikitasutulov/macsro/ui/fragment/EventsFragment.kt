d06a5064cea522a63034fece0d2d8aaff14accec
Nikita Sutulov <notsid212@gmail.com>
2025-04-24 15:31:36 +0300

    added events filtration for coordinators
    Now coordinators can only see the events they are associated with (by coordinatorGID field of Event).

--- app/src/main/java/com/nikitasutulov/macsro/ui/fragment/EventsFragment.kt
+++ app/src/main/java/com/nikitasutulov/macsro/ui/fragment/EventsFragment.kt
@@ -11,6 +11,7 @@ import com.nikitasutulov.macsro.R
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.nikitasutulov.macsro.data.dto.BaseResponse
+import com.nikitasutulov.macsro.data.dto.auth.user.UserDto
 import com.nikitasutulov.macsro.data.dto.operations.event.EventDto
 import com.nikitasutulov.macsro.data.dto.operations.eventstatus.EventStatusDto
 import com.nikitasutulov.macsro.data.dto.operations.eventtype.EventTypeDto
@@ -21,9 +22,11 @@ import com.nikitasutulov.macsro.ui.adapter.EventAdapter
 import com.nikitasutulov.macsro.utils.SessionManager
 import com.nikitasutulov.macsro.utils.handleError
 import com.nikitasutulov.macsro.utils.observeOnce
+import com.nikitasutulov.macsro.viewmodel.auth.AuthViewModel
 import com.nikitasutulov.macsro.viewmodel.operations.EventStatusViewModel
 import com.nikitasutulov.macsro.viewmodel.operations.EventTypeViewModel
 import com.nikitasutulov.macsro.viewmodel.operations.EventViewModel
+import com.nikitasutulov.macsro.viewmodel.operations.OperationWorkerViewModel
 import com.nikitasutulov.macsro.viewmodel.utils.DistrictViewModel
 
 class EventsFragment : Fragment() {
@@ -31,14 +34,18 @@ class EventsFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var sessionManager: SessionManager
     private lateinit var eventAdapter: EventAdapter
+    private lateinit var authViewModel: AuthViewModel
     private lateinit var eventViewModel: EventViewModel
     private lateinit var eventTypeViewModel: EventTypeViewModel
     private lateinit var eventStatusViewModel: EventStatusViewModel
     private lateinit var districtViewModel: DistrictViewModel
+    private lateinit var operationWorkerViewModel: OperationWorkerViewModel
     private lateinit var events: List<Event>
     private lateinit var eventTypes: Map<String, EventTypeDto>
     private lateinit var eventStatuses: Map<String, EventStatusDto>
     private lateinit var districts: Map<String, DistrictDto>
+    private lateinit var token: String
+    private lateinit var user: UserDto
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -92,39 +99,80 @@ class EventsFragment : Fragment() {
     }
 
     private fun initViewModels() {
+        authViewModel = ViewModelProvider(this)[AuthViewModel::class.java]
         eventViewModel = ViewModelProvider(this)[EventViewModel::class.java]
         eventTypeViewModel = ViewModelProvider(this)[EventTypeViewModel::class.java]
         eventStatusViewModel = ViewModelProvider(this)[EventStatusViewModel::class.java]
         districtViewModel = ViewModelProvider(this)[DistrictViewModel::class.java]
+        operationWorkerViewModel = ViewModelProvider(this)[OperationWorkerViewModel::class.java]
     }
 
     private fun fetchEvents() {
-        val token = sessionManager.getToken()
-        eventTypeViewModel.getAll("Bearer $token", null, null)
-        eventTypeViewModel.getAllResponse.observeOnce(viewLifecycleOwner) { response ->
-            if (response is BaseResponse.Success) {
-                eventTypes = response.data!!.associateBy { it.gid }
-                eventStatusViewModel.getAll("Bearer $token", null, null)
-            } else if (response is BaseResponse.Error) {
-                showFetchEventsError(response)
+        token = sessionManager.getToken()!!
+
+        authViewModel.validateToken("Bearer $token")
+        authViewModel.tokenValidationResponse.observeOnce(viewLifecycleOwner) { validationResponse ->
+            if (validationResponse is BaseResponse.Success) {
+                user = validationResponse.data!!.user!!
+                eventTypeViewModel.getAll("Bearer $token", null, null)
+                eventTypeViewModel.getAllResponse.observeOnce(viewLifecycleOwner) { response ->
+                    if (response is BaseResponse.Success) {
+                        eventTypes = response.data!!.associateBy { it.gid }
+                        eventStatusViewModel.getAll("Bearer $token", null, null)
+                    } else if (response is BaseResponse.Error) {
+                        showFetchEventsError(response)
+                    }
+                }
+                eventStatusViewModel.getAllResponse.observeOnce(viewLifecycleOwner) { response ->
+                    if (response is BaseResponse.Success) {
+                        eventStatuses = response.data!!.associateBy { it.gid }
+                        districtViewModel.getAll("Bearer $token", null, null)
+                    } else if (response is BaseResponse.Error) {
+                        showFetchEventsError(response)
+                    }
+                }
+                districtViewModel.getAllResponse.observeOnce(viewLifecycleOwner) { response ->
+                    if (response is BaseResponse.Success) {
+                        districts = response.data!!.associateBy { it.gid }
+                        if (user.roles.any { it.name == "Coordinator" }) {
+                            fetchEventsForCoordinator()
+                        } else if (user.roles.any { it.name == "Volunteer" }) {
+                            fetchEventsForVolunteer()
+                        }
+                    } else if (response is BaseResponse.Error) {
+                        showFetchEventsError(response)
+                    }
+                }
+            } else if (validationResponse is BaseResponse.Error) {
+                showFetchEventsError(validationResponse)
             }
         }
-        eventStatusViewModel.getAllResponse.observeOnce(viewLifecycleOwner) { response ->
+    }
+
+    private fun fetchEventsForCoordinator() {
+        var operationWorkerGID = ""
+        operationWorkerViewModel.getByUserGID("Bearer $token", user.id)
+        operationWorkerViewModel.getByUserGIDResponse.observeOnce(viewLifecycleOwner) { response ->
             if (response is BaseResponse.Success) {
-                eventStatuses = response.data!!.associateBy { it.gid }
-                districtViewModel.getAll("Bearer $token", null, null)
-            } else if (response is BaseResponse.Error) {
-                showFetchEventsError(response)
-            }
-        }
-        districtViewModel.getAllResponse.observeOnce(viewLifecycleOwner) { response ->
-            if (response is BaseResponse.Success) {
-                districts = response.data!!.associateBy { it.gid }
+                operationWorkerGID = response.data!!.gid
                 eventViewModel.getAll("Bearer $token", null, null)
             } else if (response is BaseResponse.Error) {
                 showFetchEventsError(response)
             }
         }
+        eventViewModel.getAllResponse.observeOnce(viewLifecycleOwner) { response ->
+            if (response is BaseResponse.Success) {
+                val eventsOfCoordinator = response.data!!.filter { it.coordinatorGID == operationWorkerGID }
+                mapEvents(eventsOfCoordinator)
+                eventAdapter.submitList(events)
+            } else if (response is BaseResponse.Error) {
+                showFetchEventsError(response)
+            }
+        }
+    }
+
+    private fun fetchEventsForVolunteer() {
+        eventViewModel.getAll("Bearer $token", null, null)
         eventViewModel.getAllResponse.observeOnce(viewLifecycleOwner) { response ->
             if (response is BaseResponse.Success) {
                 mapEvents(response.data!!)
